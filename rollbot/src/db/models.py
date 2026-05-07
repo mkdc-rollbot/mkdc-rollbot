7c3f5b0a1c1f11d7eb1657cf1fa60b9bf91f12ee
dorlo1994 <dor.lotan94@gmail.com>
2026-05-07 13:57:05 +0300

    WIP

--- rollbot/src/db/models.py
+++ rollbot/src/db/models.py
@@ -7,32 +7,37 @@ Base = declarative_base()
 class Guild(Base):
     __tablename__ = "guilds"
     id = Column(BigInteger, primary_key=True)
+    channels = relationship("Channel", back_populates="guild")
 
 
 class Channel(Base):
     __tablename__ = "channels"
     id = Column(BigInteger, primary_key=True)
     guild_id = Column(BigInteger, ForeignKey("guilds.id"))
+
     prefix = Column(String)
     system = Column(String)
 
     channel_characters = relationship("ChannelCharacter", back_populates="channel")
-
+    guild = relationship("Guild", back_populate("channels"))
 
 class Player(Base):
     __tablename__ = "players"
     id = Column(BigInteger, primary_key=True)
+    characters = relationship("Character", back_populates="player")
 
 
 class Character(Base):
-    __tablename__ "characters"
+    __tablename__ = "characters"
     id = Column(Integer, primary_key=True)
     player_id = Column(BigInteger, ForeignKey("players.id"))
 
     name = Column(String)
     sheet_data = Column(JSON)
 
-    channel_links = relationship("ChannelCharacter", backpopulates="character")
+    player = relationship("Player", back_populates="characters")
+
+    channel_links = relationship("ChannelCharacter", back_populates="character")
 
 
 class ChannelCharacter(Base):
@@ -40,7 +45,7 @@ class ChannelCharacter(Base):
     id = Column(Integer, primary_key=True)
     channel_id = Column(BigInteger, ForeignKey("channels.id"))
     player_id = Column(BigInteger, ForeignKey("players.id"))
-    character_id = Column(BigInteger, ForeignKey("characters.id"))
+    character_id = Column(Integer, ForeignKey("characters.id"))
 
     channel = relationship("Channel", back_populates="channel_characters")
     character = relationship("Character", back_populates="channel_links")
--- rollbot/src/db/repositories/channels.py
+++ rollbot/src/db/repositories/channels.py
@@ -2,3 +2,19 @@ from db.models import Channel
 
 def get_channel(session, channel_id):
     return session.get(Channel, channel_id)
+
+
+def get_or_create_channel(session, guild_id, channel_id):
+    channel = get_channel(session, channel_id)
+
+    if not channel:
+        channel = ChannelModel(
+            id=channel_id,
+            guild_id=guild_id,
+            prefix="~",
+            system=None
+        )
+        session.add(channel)
+        session.commit()
+
+    return channel
